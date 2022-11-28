fc2947af731e4be5ef386f9fe593be74b8a5e73e
GuyGoose <antokeeffe@outlook.ie>
2022-11-28 00:16:24 +0000

    Intial Commit.

---
+++ src/pages/upcomingMoviesPage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToPlaylistIcon from "../components/cardIcons/addToPlaylist";
+
+
+const UpcomingMoviesPage = (props) => {
+
+  const {  data, error, isLoading, isError }  = useQuery('discover', getUpcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  const favourites = movies.filter(m => m.favourite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
+  const addToFavourites = (movieId) => true 
+
+  return (
+    <PageTemplate
+      title="Upcoming Movies"
+      movies={movies}
+      action={(movie) => {
+        return <AddToPlaylistIcon movie={movie} />
+      }}
+    />
+);
+};
+export default UpcomingMoviesPage;
